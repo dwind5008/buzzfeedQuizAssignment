c8c0bbf8c72ea0e6474c9667a120f85e1ed90c2e
dwind5008 <davidhenrywind1989@gmail.com>
2016-12-18 15:11:54 +0200

    made all changes except adding lightbox

--- js/BuzzfeedQuiz.js
+++ js/BuzzfeedQuiz.js
@@ -1,6 +1,7 @@
 /**
  * Created by dwind on 12/14/16.
  */
+
 var sum = 0;
 var number = "";
 
@@ -11,30 +12,35 @@ function checkIfChecked(className){
     for (var i = 0; i < arr.length; i++) {
 
         if (arr[i].checked) {
-            value = parseInt(arr[i].value);
-            sum += value;
+            sum += parseInt(arr[i].value);
         }
     }
 }
 
-function checkNumberInput(){
+function checkNumberInput() {
 
-    number = parseInt(document.getElementById("c").value);
+    number = parseInt(document.getElementById("jackets").value);
 
-    if (number >= 4){
-        sum += 1;
+    if (number > 4) {
+        sum += 1
     }
-    if (number == 3){
-        sum += 2;
-    }
-    if (number == 2){
-        sum += 3;
-    }
-    if (number == 1){
-        sum += 4;
-    }
-    if (number == 0){
-        sum += 5;
+
+    else switch (number) {
+        case (4):
+            sum += 1;
+            break;
+        case (3):
+            sum += 2;
+            break;
+        case (2):
+            sum += 3;
+            break;
+        case (1):
+            sum += 4;
+            break;
+        case (0):
+            sum += 5;
+            break;
     }
 }
 
@@ -45,33 +51,32 @@ function checkIfSelected(className){
     for (var i = 0; i < arr.length; i++) {
 
         if (arr[i].selected) {
-            value = parseInt(arr[i].value);
-            sum += value;
+            sum += parseInt(arr[i].value);
         }
     }
 }
 
 function showResult () {
 
-    checkIfChecked("a");
-    checkIfChecked("b");
+    checkIfChecked("food");
+    checkIfChecked("beverage");
     checkNumberInput();
-    checkIfSelected("d");
-    checkIfSelected("e");
+    checkIfSelected("activity");
+    checkIfSelected("scenery");
 
     if (sum <= 5) {
     document.getElementById("displayResult").innerHTML = "<p>The Midwest</p>";
     }
-    if (sum > 5 && sum <=10) {
+    else if (sum > 5 && sum <=10) {
         document.getElementById("displayResult").innerHTML = "<p>The Northeast</p>";
     }
-    if (sum > 10 && sum <=15) {
+    else if (sum > 10 && sum <=15) {
         document.getElementById("displayResult").innerHTML = "<p>The West Coast</p>";
     }
-    if (sum > 15 && sum <=20) {
+    else if (sum > 15 && sum <=20) {
         document.getElementById("displayResult").innerHTML = "<p>The South</p>";
     }
-    if (sum > 20) {
+    else if (sum > 20) {
         document.getElementById("displayResult").innerHTML = "<p>The Southwest</p>";
     }
 }
